bbc52edf9f797fc08a15472a0d917166462fca8f
nivk <nivkman@gmail.com>
2022-08-25 23:36:16 +0300

    adding POST send notification based on service

--- src/routers/v1.js
+++ src/routers/v1.js
@@ -1,3 +1,20 @@
 const router = require('express')();
+const supportedServices = require('../utils/supportedServices');
+
+
+router.post('/send', async(req, res) => {
+    const service = await req.params.service.toLowerCase();
+    const method = await req.params.method.toLowerCase();
+
+    if (!service)
+        return res.status(405).json({ err: "no service has been given" });
+
+    if (!method)
+        return res.status(405).json({ err: "no method has been given" });
+
+    const selectedMethod = await (supportedServices[service].functions).find((method) => method == req.body.method)
+    const response = await selectedMethod(req.body.args);
+    res.status(200).json(response);
+})
 
 module.exports = router;
