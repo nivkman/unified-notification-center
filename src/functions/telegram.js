8439e3a68a0dd5e6e2d195397ea92af95d6f623b
nivk <nivkman@gmail.com>
2022-08-25 23:35:44 +0300

    adding telegram init function file structure

---
+++ src/functions/telegram.js
@@ -0,0 +1,13 @@
+const sendMsgToPerson = () => {
+    return 1;
+}
+
+const sendMsgToChanel = () => {
+    return 2;
+}
+
+const sendMsgToGroup = () => {
+    return 3
+}
+
+module.exports = { sendMsgToChanel, sendMsgToGroup, sendMsgToPerson };
