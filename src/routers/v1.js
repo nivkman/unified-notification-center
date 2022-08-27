fe1eb70778cc236fbb3cf4ce400fbcc8bb0f5254
Niv Kaufman <nivkman@gmail.com>
2022-08-27 04:55:04 +0300

    formaling api body - adding custom args

--- src/routers/v1.js
+++ src/routers/v1.js
@@ -5,15 +5,15 @@ const supportedPlatforms = require('../utils/supportedPlatforms');
 router.post('/send', async (req, res) => {
     const args = await req.body;
     const platform = await args.platform.toLowerCase();
-    const method = await args.method.toLowerCase();
+    const methodName = await args.method.toLowerCase();
 
     if (!platform)
         return res.status(405).json({ err: "no platform has been given" });
 
-    if (!method)
+    if (!methodName)
         return res.status(405).json({ err: "no method has been given" });
 
-    const response = await supportedPlatforms[platform].methods.find(method => method.name === req.query.method).method(args);
+    const response = await supportedPlatforms[platform].methods.find(method => method.name.toLowerCase() === methodName).method(args.custom);
     res.status(200).json(response);
 })
 
